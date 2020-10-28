0870cb77c871439e91003982b65759301b34c7c6
kevinfiero <kjf46@cornell.edu>
2020-10-28 14:21:07 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
+import Dropdown from './Dropdown.js';
 import creatures from './data.js';
 
-function App() {
-  return (
-    <>
-    <Header/>
-    <ImageList creatures={creatures}/>
-    </>
-  );
-}
+export default class App extends React.Component {
+  state = {
+    filter: 'Show All'
+  }
 
-export default App;
+  handleChange = e => {
+      this.setState({
+          filter: e.target.value
+      });
+  }
+  render() {
+    return (
+      <>
+        <Header/>
+        <div className='center flex-row'>
+          <label id='filter-label'>Keyword: </label>
+          <Dropdown handleChange = {this.handleChange}/>
+        </div>
+        <ImageList creatures={creatures} filter = {this.state.filter}/>
+      </>
+    );
+  }
+}
